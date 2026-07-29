209704da0544fce4f927d81bcb0e856ae9326bd1
kseniyaorlova82 <kseniyaorlova82@gmail.com>
2026-07-29 09:36:04 +0200

    TestBaseHomeWork6.1.1

--- src/test/java/de/demowebshop/tests/CreateAccountTests.java
+++ src/test/java/de/demowebshop/tests/CreateAccountTests.java
@@ -17,7 +17,10 @@ public class CreateAccountTests extends TestBase {
 
         type(By.id("LastName"), "Hanzii");
 
-        type(By.id("Email"), "oksana.hanzii.82@gmail.com");
+        String email =
+                "oksana" + System.currentTimeMillis() + "@gmail.com";
+
+        type(By.id("Email"), email);
 
         type(By.id("Password"), "Aa123456!");
 
@@ -27,30 +30,6 @@ public class CreateAccountTests extends TestBase {
 
         Assert.assertTrue(
                 isElementPresent(By.cssSelector(".result"))
-        );
-    }
-
-    @Test
-    public void existedUserRegisterNegativeTest() {
-
-        click(By.cssSelector(".ico-register"));
-
-        click(By.id("gender-female"));
-
-        type(By.id("FirstName"), "Oksana");
-
-        type(By.id("LastName"), "Hanzii");
-
-        type(By.id("Email"), "oksana.hanzii.82@gmail.com");
-
-        type(By.id("Password"), "Aa123456!");
-
-        type(By.id("ConfirmPassword"), "Aa123456!");
-
-        click(By.id("register-button"));
-
-        Assert.assertTrue(
-                isElementPresent(By.cssSelector(".validation-summary-errors"))
         );
     }
 }
