1279f934546f6171b915305e5958e94583bffd98
kseniyaorlova82 <kseniyaorlova82@gmail.com>
2026-08-23 10:31:30 +0200

    HomeWork10_Value_Object_Model

--- src/test/java/core/TestBase.java
+++ src/test/java/core/TestBase.java
@@ -1,41 +1,22 @@
 package core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected ApplicationManager app;
 
-    Logger logger = LoggerFactory.getLogger(TestBase.class);
-
     @BeforeMethod
-    public void setUp(Method method) {
-        logger.info("Start test: {}", method.getName());
-
+    public void setUp() {
         app = new ApplicationManager();
         app.init();
     }
 
     @AfterMethod
-    public void tearDown(ITestResult result) {
-
-        if (result.isSuccess()) {
-            logger.info("PASSED: {}", result.getMethod().getMethodName());
-        } else {
-            logger.error("FAILED: {}", result.getMethod().getMethodName());
-        }
-
+    public void tearDown() {
         if (app != null) {
             app.stop();
         }
-
-        logger.info("Stop test");
-        logger.info("*****************************************");
     }
 }
